a95d8308284ad192a780db11fab036477fdb320b
Abdelrahman Sayed <119951252+Abdelrahmansayed1@users.noreply.github.com>
2023-08-21 15:19:09 +0300

    Update env.c

--- env.c
+++ env.c
@@ -1,98 +1,62 @@
 #include "shell.h"
 
-/**
- * get_environ - returns the string array copy of our environ
- *
- * @info: Structure containing potential arguments.
- *
- * Return: Always 0
- */
-
-
-char **get_environ(info_t *info)
+int _myenv(info_t *info)
 {
-	if (!info->environ || info->env_changed)
-	{
-		info->environ = list_to_strings(info->env);
-		info->env_changed = 0;
-	}
-
-	return (info->environ);
-}
-
-
-/**
- * _setenv - Initialize a new environment variable
- *
- * @info: Structure containing potential arguments. Used to maintain
- *
- * @var: the string env var property
- *
- * @value: the string env var value
- *
- *  Return: Always 0
- */
-
-int _setenv(info_t *info, char *var, char *value)
-{
-	char *buf = NULL;
-	list_t *node;
-	char *p;
-
-	if (!var || !value)
-		return (0);
-
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-  
-	if (buf == NULL)
-		return (1);
-  
-	_strcpy(buf, var);
-	_strcat(buf, "=");
-	_strcat(buf, value);
-	node = info->env;
-  
-	while (node)
-	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
-		{
-			free(node->str);
-			node->str = buf;
-			info->env_changed = 1;
-			return (0);
-		}
-		node = node->next;
-	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
-	info->env_changed = 1;
+	print_list_str(info->env);
 	return (0);
 }
 
-
-int _unsetenv(info_t *info, char *var)
+char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
-	size_t i = 0;
-	char *p;
-
-	if (!node || !var)
-		return (0);
+	char *c;
 
 	while (node)
 	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
-		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
-			node = info->env;
-			continue;
-		}
+		c = starts_with(node->str, name);
+		if (c && *c)
+			return (c);
 		node = node->next;
-		i++;
 	}
-	return (info->env_changed);
+	return (NULL);
+}
+
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("incorrect num of args\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+int _myunsetenv (info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("few args\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+	{
+		_unsetenv(info, info->argv[i]);
+	}
+	return (0);
+}
+
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
 }
 
