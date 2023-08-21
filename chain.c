e7b90b83d414e16774332c1f9ddd870779812778
Abdelrahman Sayed <119951252+Abdelrahmansayed1@users.noreply.github.com>
2023-08-21 16:49:45 +0300

    Create chain.c

---
+++ chain.c
@@ -0,0 +1,124 @@
+#include "shell.h"
+
+int is_chain(info_t *info, char *buf, size_t *p)
+{
+	size_t i = *p;
+
+	if (buf[i] == '|' && buf[i + 1] == '|')
+	{
+		buf[i] = 0;
+		i++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[i] == '&' && buf[i + 1] == '&')
+	{
+		buf[i] = 0;
+		i++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[i] == ';') 
+	{
+		buf[i] = 0;
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*p = i;
+	return (1);
+}
+
+
+
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t k = *p;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[k] = 0;
+			k = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			k = len;
+		}
+	}
+
+	*p = k;
+}
+
+
+
+int replace_alias(info_t *info)
+{
+	int i;
+	list_t *node;
+	char *p;
+
+	for (i = 0; i < 10; i++)
+	{
+		node = node_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _strchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = _strdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
+}
+
+
+int replace_vars(info_t *info)
+{
+	int j = 0;
+	list_t *node;
+
+	for (j = 0; info->argv[j]; j++)
+	{
+		if (info->argv[j][0] != '$' || !info->argv[j][1])
+			continue;
+
+		if (!_strcmp(info->argv[j], "$?"))
+		{
+			replace_string(&(info->argv[j]),
+					_strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[j], "$$"))
+		{
+			replace_string(&(info->argv[j]),
+					_strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with(info->env, &info->argv[j][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[j]),
+					_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[j], _strdup(""));
+
+	}
+	return (0);
+}
+
+
+
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
